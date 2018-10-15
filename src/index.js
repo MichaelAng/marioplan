24c21edc2165ab52afb45647960788044f7ad1ea
Michael Ang <angmerica@gmail.com>
2018-10-15 10:02:15 -0400

    hook up profile

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const store = createStore(
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
+        reactReduxFirebase(fbConfig, { attachAuthIsReady: true, useFirestoreForProfile: true, userProfile: 'users' })
     )
 );
 
