40b405736aaafceebabbdbbd4090ac52eab05877
Michael Ang <angmerica@gmail.com>
2018-10-26 09:38:54 -0400

    populate the notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import ProjectList from '../projects/ProjectList';
 
 class Dashboard extends Component {
     render() {
-        const { auth, projects } = this.props;
+        const { auth, projects, notifications } = this.props;
 
         if (!auth.uid) {
             return <Redirect to="/signin" />;
@@ -22,7 +22,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -34,10 +34,14 @@ const mapStateToProps = state => {
     return {
         auth: state.firebase.auth,
         projects: state.firestore.ordered.projects,
+        notifications: state.firestore.ordered.notifications,
     };
 };
 
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect([{ collection: 'projects' }])
+    firestoreConnect([
+        { collection: 'projects' },
+        { collection: 'notifications', limit: 3 },
+    ])
 )(Dashboard);
